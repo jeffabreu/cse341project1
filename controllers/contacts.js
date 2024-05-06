a24916b61e5f922b986c4b6e2024b63cd30fad16
jeffersonabreu <sil21018@byui.edu>
2024-05-05 22:29:58 -0300

    swagger addition

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,25 +1,91 @@
-
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase().db().collection('contacts').find();
-    result.toArray().then((contacts) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(contacts)
-    });
+  //#swagger.tags=['Contacts']
+    try {
+        const result = await mongodb.getDatabase().db().collection('contacts').find().toArray();
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
 
 const getSingle = async (req, res) => {
-    const contactId = new ObjectId(String(req.params.id));
-    const result = await mongodb.getDatabase().db().collection('contacts').find({_id: contactId});
-    result.toArray().then((contacts) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(contacts[0])
-    });
+  //#swagger.tags=['Contacts']
+    try {
+        const contactId = new ObjectId(String(req.params.id));
+        const contact = await mongodb.getDatabase().db().collection('contacts').findOne({_id: contactId});
+        if (contact) {
+            res.status(200).json(contact);
+        } else {
+            res.status(404).json({ message: 'Contact not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const createContact = async (req, res) => {
+  //#swagger.tags=['Contacts']
+    try {
+        const contact = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            favoriteColor: req.body.favoriteColor,
+            birthday: req.body.birthday
+        };
+        const response = await mongodb.getDatabase().db().collection('contacts').insertOne(contact);
+        res.status(201).json({ message: 'Contact created successfully', contact });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const updateContact = async (req, res) => {
+  //#swagger.tags=['Contacts']
+    try {
+        const contactId = new ObjectId(String(req.params.id));
+        const contactUpdates = {
+            $set: {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                favoriteColor: req.body.favoriteColor,
+                birthday: req.body.birthday
+            }
+        };
+        const response = await mongodb.getDatabase().db().collection('contacts').updateOne({_id: contactId}, contactUpdates);
+        if (response.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(404).json({ message: 'Contact not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const deleteContact = async (req, res) => {
+  //#swagger.tags=['Contacts']
+    try {
+        const contactId = new ObjectId(String(req.params.id));
+        const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({_id: contactId});
+        if (response.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(404).json({ message: 'Contact not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
 
 module.exports={
     getAll,
-    getSingle
-}
+    getSingle,
+    createContact,
+    updateContact,
+    deleteContact
+};
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express'); //include this from the library
- 
+const bodyParser = require ('body-parser');
 const mongodb = require('./data/database');
 const app = express();
  
  const port = process.env.PORT || 3000;
+ app.use(bodyParser.json());
+ app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 
+    'Origin, x-Requested-with, Content-Type, Accept, Z-key')
+    res.setHeader('Access-Control-Allow-Origin', 'GET, POST, DELETE, OPTIONS');
+    next();
+ })
  app.use('/', require('./routes'));  
-
 
 
  mongodb.initDb(err => {
